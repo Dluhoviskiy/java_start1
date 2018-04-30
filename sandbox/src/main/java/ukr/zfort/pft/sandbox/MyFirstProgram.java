681011307d211ce19bd65f60ac3f77e616913513
Dluhoviskiy <dm.luhoviskiy@gmail.com>
2018-04-30 06:57:00 +0300

    добавил функцию

--- sandbox/src/main/java/ukr/zfort/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ukr/zfort/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,22 @@ package ukr.zfort.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main (String[] args) {
-
-	System.out.println("Hello, world");
+      hello("world");
+      hello("Alecs");
+      hello("again");
+      double l=5;
+      System.out.println( "ПЛОЩАДЬ КВАДРАТА СО СТООНОЙ " + l + " = " + area(l));
+      double a=4;
+      double b=6;
+      System.out.println( "ПЛОЩАДЬ Прямоугольника СО СТООНами " + a + " и " + b + " = " + area(a, b));
+  }
+  public static void hello(String somebody) {
+       System.out.println("Hello, " + somebody + "!");
+  }
+  public static double area(double len){
+    return len*len;
+  }
+  public static double area(double a, double b){
+        return a*b;
   }
 }
