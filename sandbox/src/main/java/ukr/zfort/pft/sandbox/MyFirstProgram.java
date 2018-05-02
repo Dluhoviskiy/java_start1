3de49e713398659921ef4479d45048073994de1f
Dluhoviskiy <dm.luhoviskiy@gmail.com>
2018-05-02 06:56:54 +0300

    Rectangle and Square

--- sandbox/src/main/java/ukr/zfort/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ukr/zfort/pft/sandbox/MyFirstProgram.java
@@ -6,19 +6,15 @@ public class MyFirstProgram {
       hello("world");
       hello("Alecs");
       hello("again");
-      double l=5;
-      System.out.println( "ПЛОЩАДЬ КВАДРАТА СО СТООНОЙ " + l + " = " + area(l));
-      double a=4;
-      double b=6;
-      System.out.println( "ПЛОЩАДЬ Прямоугольника СО СТООНами " + a + " и " + b + " = " + area(a, b));
+      Square s = new Square(5);
+
+      System.out.println( "ПЛОЩАДЬ КВАДРАТА СО СТООНОЙ " + s.l + " = " + s.area());
+      Rectangle r = new Rectangle(4, 6);
+        System.out.println( "ПЛОЩАДЬ Прямоугольника СО СТООНами " + r.a + " и " + r.b + " = " + r.area());
   }
   public static void hello(String somebody) {
        System.out.println("Hello, " + somebody + "!");
   }
-  public static double area(double len){
-    return len*len;
-  }
-  public static double area(double a, double b){
-        return a*b;
-  }
+
+
 }
