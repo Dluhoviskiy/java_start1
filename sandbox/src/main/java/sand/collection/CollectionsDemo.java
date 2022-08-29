e962cd4df4c8ba72f1c17a055a9f66bccc7f6ea6
Dluhoviskiy <dm.luhoviskiy@gmail.com>
2022-08-29 12:08:03 +0300

    First commit army 2 Second

---
+++ sandbox/src/main/java/sand/collection/CollectionsDemo.java
@@ -0,0 +1,29 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+
+public class Collections {
+
+ public static void main(String[] args) {
+
+//  var list = new ArrayList<String>();
+//
+//  list.addAll(Arrays.asList("", "a", "b", "c"));
+//  list.add("d");
+//
+//  System.out.println("list = " + list);
+
+  Collection<String> collection = new ArrayList<>();
+  Collections.addAll(collection, "a");
+//  var list = new ArrayList<String>();
+//
+//  list.addAll(Arrays.asList("", "a", "b", "c"));
+//  ints.add("a");
+
+  System.out.println("list = " + ints);
+ }
+}
